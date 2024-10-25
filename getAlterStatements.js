f905043972fa3faa53fc482242a356c695347165
SemProvoost <27961543+SemProvoost@users.noreply.github.com>
2024-10-25 16:43:37 +0200

    add create statements

--- getAlterStatements.js
+++ getAlterStatements.js
@@ -6,14 +6,17 @@ function getAlterStatements(oldSchema, newSchema) {
 
     const alterStatements = [];
     const modifyStatements = [];
+    const createStatements = [];
     const debugDiffs = [];
 
     for(const tableName in newSchemaJson){
-        const newTable = newSchemaJson[tableName];
-        const oldTable = oldSchemaJson[tableName];
+        const newTable = newSchemaJson[tableName]?.['create'];
+        const newTableAlterRows = newSchemaJson[tableName]?.['alterRows'];
+        const oldTable = oldSchemaJson[tableName]?.['create'];
 
         if(!oldTable){
-            console.log(`Table ${tableName} does not exist in old schema`);
+            // table doesn't exist in the old schema, so it's a new table
+            createStatements.push(createTable(tableName, newTableAlterRows, newTable));
             continue;
         }
 
@@ -35,6 +38,7 @@ function getAlterStatements(oldSchema, newSchema) {
     }
 
     fs.writeFileSync('./output/alter_statements.sql', alterStatements.join('\n'));
+    fs.writeFileSync('./output/create_statements.sql', createStatements.join('\n'));
     fs.writeFileSync('./output/modify_statements.sql', modifyStatements.join('\n'));
     fs.writeFileSync('./output/modify_statements_debug.txt', debugDiffs.join('\n'));
 
@@ -54,33 +58,80 @@ function schemaToJson(schema){
 
     const lines = schema.split('\n');
 
-    let bracketOpen = false;
+    let createOpen = false;
+    let alterOpen = false;
     let tableName = '';
 
     for(const line of lines){
 
-        if(line.includes('CREATE TABLE')){
-            bracketOpen = true;
+        if(line.includes('CREATE TABLE ')){
+            createOpen = true;
             tableName = line.split('`')[1];
-            schemaJson[tableName] = {};
+            schemaJson[tableName] = {
+                create: {},
+                alterRows: [],
+            };
+            continue;
         }
-        else if(line.includes(') ENGINE')){
-            bracketOpen = false;
+        if(createOpen && line.includes(') ENGINE')){
+            createOpen = false;
+            continue;
         }
-        else if(bracketOpen){
+
+        if(line.includes('ALTER TABLE')){
+            alterOpen = true;
+            tableName = line.split('`')[1];
+            continue;
+        }
+        if(alterOpen){
+            schemaJson[tableName]['alterRows'].push(line);
+            if(line.trim().endsWith(';')){
+                alterOpen = false;
+            }
+            continue;
+        }
+
+        
+        if(createOpen){
             if(line.includes('`')){
                 const [_, column, ...rest] = line.split('`');
                 let typeStr = rest.join(' ').trim();
                 if(typeStr.endsWith(',')){
                     typeStr = typeStr.slice(0, -1);
                 }
-                schemaJson[tableName][column] = typeStr;
+                schemaJson[tableName]['create'][column] = typeStr;
             }
+            continue;
         }
 
     }
 
     return schemaJson;
+}
+
+function createTable(tableName, newTableAlterRows, newTable){
+    const createStatements = [];
+    createStatements.push('');
+    createStatements.push(`CREATE TABLE \`${tableName}\` (\n${Object.entries(newTable).map(([column, type]) => `  \`${column}\` ${type}`).join(',\n')}\n);`);
+    if(newTableAlterRows){
+        let alterStr = `ALTER TABLE \`${tableName}\`\n`;
+        let isAlterOpen = true;
+        for(const alterRow of newTableAlterRows){
+            console.log(tableName, newTableAlterRows)
+            if(!isAlterOpen){
+                // alter not open anymore, but more alter statements are there
+                // let's add the alter table again
+                alterStr += `ALTER TABLE \`${tableName}\`\n`;
+                isAlterOpen = true;
+            }
+            alterStr += alterRow+'\n';
+            if(alterRow.trim().endsWith(';')){
+                isAlterOpen = false;
+            }
+        }
+        createStatements.push(alterStr);
+    }
+    return createStatements.join('\n');
 }
 
 const fs = require('fs');
